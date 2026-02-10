e9ca47bcbb2f6140984013fdd2606a3a092dd753
Yossy Adirta <yossyadirta@gmail.com>
2026-02-10 22:59:37 +0700

    feat(ui): create option dropdown commponent

---
+++ src/components/ui/option-dropdown.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Ellipsis, PencilIcon, TrashIcon } from "lucide-react";
+
+type Props = {
+  open: boolean;
+  onOpenChange: () => void;
+  onDelete: () => void;
+  onUpdate: () => void;
+  btnSize?: "default" | "sm" | "xs";
+};
+
+export function OptionDropdown({
+  open,
+  onOpenChange,
+  onDelete,
+  onUpdate,
+  btnSize,
+}: Props) {
+  return (
+    <DropdownMenu open={open} onOpenChange={onOpenChange}>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size={btnSize}>
+          <Ellipsis />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-40" align="end">
+        <DropdownMenuItem onClick={onUpdate}>
+          <PencilIcon />
+          Edit
+        </DropdownMenuItem>
+        <DropdownMenuGroup>
+          <DropdownMenuItem variant="destructive" onClick={onDelete}>
+            <TrashIcon />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
