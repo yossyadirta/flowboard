4267108f737d403533216d2b958cc48e9b1de6d7
Yossy Adirta <yossyadirta@gmail.com>
2026-02-18 21:20:02 +0700

    chore(state): add modall state

--- src/types/state.ts
+++ src/types/state.ts
@@ -1,7 +1,17 @@
 import { Board } from "./board";
-import { Task } from "./task";
+import { Task, TaskStatus } from "./task";
 
 export type AppState = {
   boards: Record<string, Board>;
   tasks: Record<string, Task>;
 };
+
+export type ModalState =
+  | { type: null }
+  | { type: "option-board" }
+  | { type: "delete-board" }
+  | { type: "edit-board" }
+  | { type: "option-task"; taskId: string }
+  | { type: "delete-task"; taskId: string }
+  | { type: "add-task"; status?: TaskStatus | undefined; boardId: string }
+  | { type: "edit-task"; data: Task };
